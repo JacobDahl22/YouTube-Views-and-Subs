5c30582c2bc03715e55be506cf3bcb33081fb49b
JacobDahl22 <84434496+JacobDahl22@users.noreply.github.com>
2021-05-18 23:21:05 -0700

    Add files via upload

---
+++ ShowYTSubs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#---------------------------------------------------
+#AUTHOR: jacobdahl09@gmail.com
+#DATE CREATED: May 18, 2021 11:03pm
+#DESCRIPTION: A simple code that uses the YouTube API in order to show the current subscriber count of whatever YouTube channel is put into the script
+#---------------------------------------------------
+
+#Necessary imports
+import json
+import urllib.request
+from win10toast import ToastNotifier
+
+channelID = "UC3CAbRsxYUnmmRAewQfYpdA" #The channel ID of whatever channel you want to see the subscriber count of
+
+APIkey = "AIzaSyD7p4mLZcujeQNoDwWbHb6q48DguTSrIsw" #Your API keyfrom Google's API service
+
+openSite = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id="+channelID+"&key="+APIkey).read()
+
+channelSubs = json.loads(openSite)["items"][0]["statistics"]["subscriberCount"] #Finds the subscriber count of the channel
+channelViews = json.loads(openSite)["items"][0]["statistics"]["viewCount"] #Finds the view count of the channel
+
+toasterNotif = ToastNotifier() #Creates a popup on your computer
+
+message = ("You have %d" %int(channelSubs) +" subscribers and %d" %int(channelViews) + " views.") #What the ToastNotifier is displaying
+
+toasterNotif.show_toast("Pr1me", message) #The popup name followed by the message containing the number of subscribers
